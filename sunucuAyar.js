c5e500af2d30798c77b76003998d56f25bfc4157
AlperenÇ <77838443+NoMercyJS@users.noreply.github.com>
2021-02-26 15:18:09 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '813401867920474153', // sunucu idsi
+  ownerRole:"814228282584989736", // sunucu sahiplerinin rolü
+  teyitsizRolleri:["814541519293513809"], // kayıt olmayanlara verieln rol
+  jailRolu:"814541519293513809", // cezalılara verilen rol
+  enAltYetkiliRolu:["814482229581447192"], // ilk yetkili permi
+  activity:"No Mercy♥ Suny", // botun durumu
+  status:"online",
+  invitelink:"discord.gg/fSdGqNdaXY", //davet linki
+  symbol:"Suny", // tag
+  voicechannel:"814587197939777546" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
